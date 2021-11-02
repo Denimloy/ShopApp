b6bc2731166a84087b595625046c1dd4424ab931
Denimloy <klochandenis@gmail.com>
2021-11-02 20:48:56 +0200

    Refactored error handler

---
+++ Extensions/EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using ShopApp.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ShopApp.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            try
+            {
+                return value.GetType()
+                    .GetMember(value.ToString())
+                    .First()
+                    .GetCustomAttribute<DescriptionAttribute>()
+                    .Description;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
--- Services/ErrorServis.cs
+++ Services/ErrorServis.cs
@@ -1,29 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ShopApp.Enums;
 using ShopApp.ViewModels;
+using ShopApp.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace ShopApp.Services
 {
     public class ErrorServis
     {
+        private readonly ILogger _logger;
+        public ErrorServis(ILogger<ErrorServis> logger)
+        {
+            _logger = logger;
+        }
         public async Task<ErrorViewModel> HandleErrorAsync(int statusCode)
         {
             return await Task.Run(() => HandleError(statusCode));
         }
         private ErrorViewModel HandleError(int statusCode)
         {
+
             ErrorStatusCodes value = (ErrorStatusCodes)statusCode;
 
             ErrorViewModel model = new();
             model.StatusCode = statusCode;
-            model.StatusCodeDescription = value.GetDescription();
-
-            if(statusCode == 404)
+            model.StatusCodeDescription = value.GetDescription() ?? "Something went wrong";
+            if(model.StatusCodeDescription == "Something went wrong")
+            {
+                _logger.LogError("Error status code not found: {statusCode}", statusCode);
+            }
+            if (statusCode == 404)
             {
                 model.ErrorMessage = "We could not find the page you were looking for.";
             }
@@ -39,16 +45,4 @@ namespace ShopApp.Services
             return model;
         }
     }
-    public static class EnumExtensions
-    {
-            public static string GetDescription(this Enum enumValue)
-            {
-                return enumValue.GetType()
-                           .GetMember(enumValue.ToString())
-                           .First()
-                           .GetCustomAttribute<DescriptionAttribute>()
-                           .Description;
-            }
-    }
-
 }
